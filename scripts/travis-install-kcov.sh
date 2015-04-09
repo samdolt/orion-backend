7b293f0e4e18c70a1b047b77c22b8d882e827bc2
Samuel Dolt <samuel@dolt.ch>
2015-04-09 13:25:59 +0200

    Fix kcov deps on travis-install-kcov.sh

--- scripts/travis-install-kcov.sh
+++ scripts/travis-install-kcov.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-sudo apt-get install libcurl4-openssl-dev libelf-dev libdw-dev binutils-dev
+sudo apt-get install libdw-dev libelf-dev elfutils libcurl4-openssl-dev cmake binutils-dev
 wget https://github.com/SimonKagstrom/kcov/archive/v26.tar.gz
 tar xzf v26.tar.gz
 mkdir kcov-26/build
