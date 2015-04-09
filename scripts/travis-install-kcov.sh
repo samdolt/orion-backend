6a3c88ec0bfe2db48f468c310248616e5509628f
Samuel Dolt <samuel@dolt.ch>
2015-04-09 13:12:22 +0200

    Use kcov v.26 instead of master

--- scripts/travis-install-kcov.sh
+++ scripts/travis-install-kcov.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
-sudo apt-get install libcurl4-openssl-dev libelf-dev libdw-dev &&
-wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz &&
-tar xzf master.tar.gz &&
-mkdir kcov-master/build &&
-cd kcov-master/build &&
-cmake .. &&
-make &&
-sudo make install &&
+sudo apt-get install libcurl4-openssl-dev libelf-dev libdw-dev binutils-dev libiberty-dev
+wget https://github.com/SimonKagstrom/kcov/archive/v26.tar.gz
+tar xzf v26.tar.gz
+mkdir kcov-26/build
+cd kcov-26/build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make
+sudo make install
 cd ../..
