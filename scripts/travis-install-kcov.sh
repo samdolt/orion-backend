d540668a8d111f4b25fb5c651fb6197f0de230a6
Samuel Dolt <samuel@dolt.ch>
2015-04-09 13:44:00 +0200

    Another fix for kcov on travis.ci

--- scripts/travis-install-kcov.sh
+++ scripts/travis-install-kcov.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-sudo apt-get install libdw-dev libelf-dev elfutils libcurl4-openssl-dev cmake binutils-dev
+sudo apt-get install libdwarf-dev libdw-dev libelf-dev elfutils libcurl4-openssl-dev cmake binutils-dev
 wget https://github.com/SimonKagstrom/kcov/archive/v26.tar.gz
 tar xzf v26.tar.gz
 mkdir kcov-26/build
